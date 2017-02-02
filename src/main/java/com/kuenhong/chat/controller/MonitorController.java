1a07810697289ca42c1420a83812a602fb1d010a
kuen-hong <kuen3824@gmail.com>
2017-02-02 16:57:59 +0800

    fix index page style, and check userId when visit chat page(not finished).

--- src/main/java/com/kuenhong/chat/controller/MonitorController.java
+++ src/main/java/com/kuenhong/chat/controller/MonitorController.java
@@ -1,7 +1,9 @@
 package com.kuenhong.chat.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,7 +16,10 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kuenhong.chat.bean.ActiveUser;
 import com.kuenhong.chat.bean.Message;
@@ -35,6 +40,20 @@ public class MonitorController {
 		model.addAttribute("name", "Jackson");
 		model.addAttribute("books", Arrays.asList("Spring 4", "Spring Boot 1.4.0", "Websocket"));
 		return "monitor";
+	}
+	
+	@ResponseBody
+	@PostMapping("/existOrNot")
+	public Map<String, Object> checkNicknameInRepository(@RequestBody ActiveUser joinUser) {
+		Map<String, Object> returnMap = new HashMap<>();
+		returnMap.put("status", Boolean.FALSE);
+		for (Map.Entry<String, String> entry : activeUserRepository.getActiveSessions().entrySet()) {
+			if (entry.getValue().equals(joinUser.getUserId())) {
+				returnMap.put("status", Boolean.TRUE);
+				break;
+			}
+		}
+		return returnMap;
 	}
 	
 	@SendTo("/topic/activeUsers")
